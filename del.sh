6f0d59a1b1e84b8460467f59008ef406216316d2
Was321 <96862791+Was321@users.noreply.github.com>
2021-12-30 14:27:04 +0530

    updated echo command

--- del.sh
+++ del.sh
@@ -2,6 +2,6 @@
 mkdir server1
 cd server1
 touch webserver.txt
-echo "hsdjhsajdhksajhdkjh" >> webserver.txt
+echo "hi hsdjhsajdhksajhdkjh" >> webserver.txt 
 chmod 665 webserver.txt
 mv webserver.txt appserver.txt
